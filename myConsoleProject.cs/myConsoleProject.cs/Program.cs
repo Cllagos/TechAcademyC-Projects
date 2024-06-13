620ca0d9072b34052e753ea8f764fd7c1e0f075d
Carlos Lagos <carloslagos85@outlook.com>
2024-06-13 14:06:27 -0500

    Commiting My First Console Project 061324

---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();*/
+
+            Console.WriteLine("Welcom To ACME Accounting Systems! \r");
+            Console.WriteLine("Remember, We're \"Accounting\" On You!");
+            Console.ReadLine();
+        }
+
+    }
+
+}
