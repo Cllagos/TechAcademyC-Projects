d2ef7d368a39c3106764cf09784576a233ddce99
Cllagos <carloslagos85@outlook.com>
2024-06-17 22:04:00 -0500

    Updating TwentyOne and Calling Methods Projects 061724

---
+++ CallingMethod/CallingMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate numberCalc = new Calculate();
+            Console.WriteLine("Please Assign A Number to Multiply.");
+
+            int newNumber = Convert.ToInt32(Console.ReadLine());
+            numberCalc.Calc1 = newNumber;
+            numberCalc.Calc2 = newNumber;
+            numberCalc.Calc3 = newNumber;
+
+            Console.WriteLine(numberCalc.Calculate1(numberCalc.Calc1));
+            Console.WriteLine(numberCalc.Calculate2(numberCalc.Calc2));
+            Console.WriteLine(numberCalc.Calculate3(numberCalc.Calc3));
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
