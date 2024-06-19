f0ad8d64fcca539d78dc317fa02d2ef024d3740e
Cllagos <carloslagos85@outlook.com>
2024-06-19 12:40:50 -0500

    Struct Assignment 061924

---
+++ Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type in a number.");
+            decimal newNumber = Convert.ToDecimal(Console.ReadLine());
+
+            Number structNum = new Number();
+
+            structNum.Amount = newNumber;
+
+            Console.WriteLine("Struct Number: {0}", structNum.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
