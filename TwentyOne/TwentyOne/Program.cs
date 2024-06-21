c51cff9f5286a4a1d7be630c12f3b36a279f50b2
Cllagos <carloslagos85@outlook.com>
2024-06-21 00:55:43 -0500

    Commiting Latest Updates to Twenty One 062124

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Casino;
+using Casino.TwentyOne;
 
 namespace TwentyOne
 {
@@ -21,6 +23,13 @@ namespace TwentyOne
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using(StreamWriter file = new StreamWriter(@"C:\Users\clagos\Documents\VisualStudioLog\log.txt",true))
+
+                {
+                    file.WriteLine(player.Id);
+                }
+                
                 Game game = new TwentyOneGame();            // Polymorphism
                 game += player;
                 player.isActivelyPlaying = true;
